c8eaa1641333555b93ed7d320766a7dbc025bde6
chillindylan50 <chillindylan50@users.noreply.github.com>
2017-06-01 11:56:12 -0400

    Update plotClouds
    1. plotClouds
    a. Add subtractInitial option. This subtracts the initial value of each
    sensor from its data, from row specified by initialRow.

--- plotClouds.m
+++ plotClouds.m
@@ -1,12 +1,12 @@
 function [  ] = plotClouds( cellArrayOfData, xColumn, yColumn, inBinLower, binInc, inBinUpper, varargin )
 % DATAANALYSIS Plot clouds (confidence intervals.)
-%   [ statusFlag ] = DATAANALYSIS(
+%   [ ] = PLOTCLOUDS(
 %   cellArrayOfData, xCol, yCol, inBinLower, binInc, inBinUpper ) generates
 %   information for the patches that can be plotted as clouds.
 %
-%   Required Inputs: 
+%   Required Inputs:
 %   cellArrayOfData- cell array with your data,
-%   xCol, yCol - colum number containing x,y data, 
+%   xCol, yCol - colum number containing x,y data,
 %   inBinLower - Lower bound of your x data
 %   binInc - width of each bin
 %   inBinUpper - upper bound of your x data
@@ -36,6 +36,7 @@ addRequired(p, 'binInc', @isnumeric)
 addRequired(p, 'inBinUpper', @isnumeric)
 addOptional(p, 'initialRow', 1, @isnumeric)
 
+
 defaultColor = false; % If color not specified, let MATLAB decide a default color
 isCharOrNumeric = @(x) ischar(x) + isnumeric(x);
 addParameter(p, 'color', defaultColor, isCharOrNumeric)
@@ -60,7 +61,7 @@ sensorData = cellArrayOfData{1}(:,yColumn);
 
 binEdges = inBinLower:binInc:inBinUpper;   % Define bins
 nBins = length(binEdges); % Print bins
-binsOfData = cell(nBins, 1);    % Initialize cell array for the Bins 
+binsOfData = cell(nBins, 1);    % Initialize cell array for the Bins
 k = 1; % counter for which row in BinsF to add next row to
 b = 0; % counter for which bin you're at
 
@@ -70,10 +71,10 @@ for row = 1:size(allDataSortF, 1)
         if tempRow(xColumn)>binEdges(b + 1) % If current xCol value is greater than next binEdge
             k = 1; %Restart count
             b = b + 1;
-        end        
+        end
     end
     binsOfData{b}(k, :) = tempRow; % Add this row to the current bin
-    k = k + 1; % increment 
+    k = k + 1; % increment
 end
 
 
@@ -85,20 +86,20 @@ binCount = zeros(1, nBins);
 validRow = 1; % counter for how many valid (containing more than 3 data points) rows we have
 
 for b = 1:nBinsM1
-   binCount(b) = size(binsOfData{b}, 1);
-   
-   % If the bin is nearly empty, move on to next bin
-   if binCount(b) < 3
-       continue % Skip rest of current cycle of this for loop
-   end
-   
-   % If the bin has valid elements, then compute statistics
-   binStatistics(validRow, 1) = mean(binsOfData{b}(:, yColumn));
-   binStatistics(validRow, 2) = std(binsOfData{b}(:, yColumn));
-%    FF(b,3) = tinv(0.975,binCount-1)*FF(b,2);   % 95%CI
+    binCount(b) = size(binsOfData{b}, 1);
+    
+    % If the bin is nearly empty, move on to next bin
+    if binCount(b) < 3
+        continue % Skip rest of current cycle of this for loop
+    end
+    
+    % If the bin has valid elements, then compute statistics
+    binStatistics(validRow, 1) = mean(binsOfData{b}(:, yColumn));
+    binStatistics(validRow, 2) = std(binsOfData{b}(:, yColumn));
+    %    FF(b,3) = tinv(0.975,binCount-1)*FF(b,2);   % 95%CI
     binStatistics(validRow, 3) = 2.26*binStatistics(validRow, 2);   % 95%CI % HARDCODED
-   binMeans(validRow) = mean(binsOfData{b}(:, xColumn));
-   validRow = validRow + 1;
+    binMeans(validRow) = mean(binsOfData{b}(:, xColumn));
+    validRow = validRow + 1;
 end
 meanBinCount = mean(binCount) % Print mean number of elements in the bins
 binMeans = binMeans(1:validRow - 1); % (Implementation note: validRow starts at 1, and is incremented by one every time.)
@@ -111,8 +112,12 @@ boundsF(2, :) = binStatistics(:, 1) - binStatistics(:, 3); % mean-95%CI
 [xPatches, yPatches] = getPatches(boundsF(1, :),boundsF(2, :),binMeans);
 
 % Plot the cloud
-patch(xPatches, p.Results.yConstant*(yPatches - sensorData(p.Results.initialRow)), p.Results.color, 'FaceAlpha', 0.35, 'edgeColor', 'none');
-plot(binMeans, p.Results.yConstant*(binStatistics(:, 1) - sensorData(p.Results.initialRow)), '--', 'Color', p.Results.color, 'LineWidth', 2.0, 'DisplayName', 'Mean');
-
+    patch(xPatches, p.Results.yConstant*(yPatches - sensorData(p.Results.initialRow)), p.Results.color, 'FaceAlpha', 0.35, 'edgeColor', 'none');
+    plot(binMeans, p.Results.yConstant*(binStatistics(:, 1) - sensorData(p.Results.initialRow)), '--', 'Color', p.Results.color, 'LineWidth', 2.0, 'DisplayName', 'Mean');
+else
+    patch(xPatches, p.Results.yConstant*(yPatches), p.Results.color, 'FaceAlpha', 0.35, 'edgeColor', 'none');
+    plot(binMeans, p.Results.yConstant*(binStatistics(:, 1)), '--', 'Color', p.Results.color, 'LineWidth', 2.0, 'DisplayName', 'Mean');
+    
+end
 end
 
